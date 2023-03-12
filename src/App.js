b88e3829eefd3c25584e92b95ca3a54649fbb9f5
Annie <ananya2003pal@gmai.com>
2023-03-12 21:19:23 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import About from "./Component/About/About";
 import Chart from "./Component/Chart/Chart";
 import Home from "./Component/Home/Home";
 import Navbar from "./Component/Navbar/Navbar";
 import Contact from "./Component/Contact/Contact";
+import Login from "./Component/Login/Login";
 function App() {
-  return (
-    <>
+  function HomePage()
+  {
+    return <>
       <Navbar/>
       <Home/>
       <About/>
       <Chart/>
       <Contact/>
     </>
+  }
+  return (
+    <Router>
+      <Routes>
+      <Route path="/" element={<HomePage/>} />
+        <Route path="/login" element={<Login/>} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Component/Login/Login.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Login.css";
+export default function Login()
+{
+    return <section id="login">
+        <h1>Get Vaccinated</h1>
+            <img src="Image/home.png" alt=""/>
+            <button className="btn btn-lg btn-dark">Sign in with Google</button>
+    </section>
+}
