390b5c54de6099dd45c95c7776a672c3c75d34cd
Sabarno Biswas <sabarnobiswas2002@gmail.com>
2023-03-13 03:15:41 +0530

    Updated firebaseConfig.js

--- src/Component/Login/firebaseConfig.js
+++ src/Component/Login/firebaseConfig.js
@@ -7,13 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyA9_2iZ192NdbsvYFOrKydmv-wZke2TVck",
-    authDomain: "vaccination-website-28ba1.firebaseapp.com",
-    projectId: "vaccination-website-28ba1",
-    storageBucket: "vaccination-website-28ba1.appspot.com",
-    messagingSenderId: "390034218756",
-    appId: "1:390034218756:web:9513a91f97d4b290097a51",
-    measurementId: "G-ZJNB80L7XF"
+    //your Own details
 };
 
 // Initialize Firebase
