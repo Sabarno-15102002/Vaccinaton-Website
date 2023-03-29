bc1d6afc7a86c683eec14b3a6ffd1fea6e412541
Shubham <shubham.paul2002@gmail.com>
2023-03-29 23:00:09 +0530

    Vaccine Page Changes

--- src/Component/Infant/Vaccine.jsx
+++ src/Component/Infant/Vaccine.jsx
@@ -1,13 +1,37 @@
 import React from "react";
+import { app } from "../Login/firebaseConfig";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+import Swal from 'sweetalert2';
 
 export default function Vaccine(props)
 {
 
-    const handleClick = () => {
-        if(props.user){
-            console.log(props.user);
-        }else{
-            console.log("No User");
+    // Initialize Cloud Firestore and get a reference to the service
+    const db = getFirestore(app);
+
+    const handleClick = async () => {
+        document.getElementById("vaccine_date").setAttribute("min", new Date().toISOString().split("T")[0]);
+        const registeredDate = document.getElementById("vaccine_date").value;
+        if(registeredDate !== ""){
+            try {
+                const docRef = await addDoc(collection(db, "users"), {
+                  user: props.user.uid,
+                  vaccine: props.diseaseName,
+                  date: registeredDate
+                });
+                console.log("Document written with ID: ", docRef.id);
+                Swal.fire(
+                    'Good job!',
+                    `You have successfully registered for ${props.diseaseName} vaccine on ${registeredDate}!`,
+                    'success'
+                  )
+                document.getElementById("${props.diseaseName}").classList.remove("btn-dark");
+                document.getElementById("${props.diseaseName}").classList.add("btn-success");
+                document.getElementById("${props.diseaseName}").innerText = `Registered On ${registeredDate}`;
+                document.getElementById("${props.diseaseName}").ariaDisabled = true;
+            } catch (e) {
+                console.error("Error adding document: ", e);
+            }
         }
     }
 
@@ -16,8 +40,8 @@ export default function Vaccine(props)
         <p><strong>Dose:</strong>{props.dose}</p>
         <p><strong>Protects against:</strong>{props.ProtectsAgainst}</p>
         <p><strong>To be given:</strong>{props.toBeGiven}</p>
-        <p><strong>How can you help protect your child?</strong>:{props.protection}</p>
-        <button onClick={handleClick} className="btn btn-dark btn-lg">Register</button>
+        <p><strong>How can you help protect your child?</strong>{props.protection}</p>
+        <button onClick={handleClick} id="${props.diseaseName}" className="btn btn-dark btn-lg">Register On <input type="date" id="vaccine_date" name="vaccine_date"></input></button>
         <hr style={{width:"10%"}}/>
     </div>
 }
