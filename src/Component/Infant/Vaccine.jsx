dcd04d681f9be83b9864f96265cc49c0f9e75bdf
Shubham <shubham.paul2002@gmail.com>
2023-03-30 23:29:07 +0530

    Vaccine Page Last Changes

--- src/Component/Infant/Vaccine.jsx
+++ src/Component/Infant/Vaccine.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-//import { app } from "../Login/firebaseConfig";
+import { app } from "../Login/firebaseConfig";
 import { getFirestore, collection, addDoc } from "firebase/firestore";
-import { db } from "../Login/firebaseConfig.js"
 import Swal from 'sweetalert2';
 
 export default function Vaccine(props)
 {
 
-    
+    // Initialize Cloud Firestore and get a reference to the service
+    const db = getFirestore(app);
 
-    const handleClick = async () => {
-        document.getElementById("vaccine_date").setAttribute("min", new Date().toISOString().split("T")[0]);
-        const registeredDate = document.getElementById("vaccine_date").value;
+    const handleClick = async (event) => {
+        Array.from(document.getElementsByClassName("vaccine_date")).forEach(element => {
+            element.setAttribute("min", new Date().toISOString().split("T")[0]);
+        });
+        const registeredDate = document.getElementById(`${props.diseaseName}`).value;
         if(registeredDate !== ""){
             try {
                 const docRef = await addDoc(collection(db, "users"), {
@@ -25,10 +27,10 @@ export default function Vaccine(props)
                     `You have successfully registered for ${props.diseaseName} vaccine on ${registeredDate}!`,
                     'success'
                   )
-                document.getElementById("${props.diseaseName}").classList.remove("btn-dark");
-                document.getElementById("${props.diseaseName}").classList.add("btn-success");
-                document.getElementById("${props.diseaseName}").innerText = `Registered On ${registeredDate}`;
-                document.getElementById("${props.diseaseName}").ariaDisabled = true;
+                event.target.classList.remove("btn-dark");
+                event.target.classList.add("btn-success");
+                event.target.innerText = `Registered On ${registeredDate}`;
+                event.target.ariaDisabled = true;
             } catch (e) {
                 console.error("Error adding document: ", e);
             }
@@ -41,7 +43,7 @@ export default function Vaccine(props)
         <p><strong>Protects against:</strong>{props.ProtectsAgainst}</p>
         <p><strong>To be given:</strong>{props.toBeGiven}</p>
         <p><strong>How can you help protect your child?</strong>{props.protection}</p>
-        <button onClick={handleClick} id="${props.diseaseName}" className="btn btn-dark btn-lg">Register On <input type="date" id="vaccine_date" name="vaccine_date"></input></button>
+        <button onClick={handleClick} className="btn btn-dark btn-lg">Register On <input type="date" id={props.diseaseName} className="vaccine_date" name="vaccine_date"></input></button>
         <hr style={{width:"10%"}}/>
     </div>
 }
